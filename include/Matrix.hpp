e2ffe21fef1d3a22470dcf2d0e5953dcf7ec81a1
Elviss Strazdins <elviss@elviss.lv>
2021-01-16 18:11:12 +0200

    Add constructor for value initialization

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -6,13 +6,21 @@
 
 namespace math
 {
-    template <typename T, std::size_t C, std::size_t R> class Matrix final
+    template <typename T, std::size_t C, std::size_t R = C> class Matrix final
     {
     public:
 #if defined(__SSE__)
         alignas((C == 4 && R == 4) ? 4 * sizeof(T) : alignof(T))
 #endif
         std::array<T, C * R> m{}; // row-major matrix (transformation is pre-multiplying)
+
+        constexpr Matrix() noexcept {}
+
+        template <typename ...A>
+        explicit constexpr Matrix(A... args) noexcept:
+            m{static_cast<T>(args)...}
+        {
+        }
 
         T& operator[](std::size_t index) noexcept { return m[index]; }
         constexpr T operator[](std::size_t index) const noexcept { return m[index]; }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,22 @@
 #include "catch2/catch.hpp"
 #include "Matrix.hpp"
 
-TEST_CASE("Matrix", "matrix")
+TEST_CASE("Matrix zero initalization", "matrix")
 {
-    math::Matrix<float, 4, 4> matrix;
+    math::Matrix<float, 4> matrix;
     for (std::size_t i = 0; i < 16; ++i)
         REQUIRE(matrix[i] == 0.0F);
+}
+
+TEST_CASE("Matrix value initalization", "matrix")
+{
+    math::Matrix<float, 2, 2> matrix{
+        0.0F, 1.0F,
+        2.0F, 3.0F
+    };
+
+    REQUIRE(matrix[0] == 0.0F);
+    REQUIRE(matrix[1] == 1.0F);
+    REQUIRE(matrix[2] == 2.0F);
+    REQUIRE(matrix[3] == 3.0F);
 }
