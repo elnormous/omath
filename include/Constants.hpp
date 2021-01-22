9980ee8e7403a73b745e4d8d3a5ce5bab2eff2dd
Elviss Strazdins <elviss@elviss.lv>
2021-01-22 02:42:03 +0200

    Add constants file

---
+++ include/Constants.hpp
@@ -0,0 +1,14 @@
+//
+// elnormous/math
+//
+
+#ifndef MATH_CONSTANTS
+#define MATH_CONSTANTS
+
+namespace math
+{
+    template <typename T> constexpr T tau = T(6.28318530717958647692);
+    template <typename T> constexpr T pi = T(3.14159265358979323846);
+}
+
+#endif
