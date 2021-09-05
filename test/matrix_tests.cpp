a541fd48c2362021e943283e33062bffe591f1a8
Elviss Strazdins <elviss@elviss.lv>
2021-09-05 09:54:02 +0300

    Compare against matrices in tests

--- test/matrix_tests.cpp
+++ test/matrix_tests.cpp
@@ -16,20 +16,20 @@ TEST_CASE("Matrix value initalization", "matrix")
         2.0F, 3.0F
     };
 
-    REQUIRE(matrix[0][0] == 0.0F);
-    REQUIRE(matrix[0][1] == 1.0F);
-    REQUIRE(matrix[1][0] == 2.0F);
-    REQUIRE(matrix[1][1] == 3.0F);
+    REQUIRE(matrix == omath::Matrix<float, 2, 2>{
+        0.0F, 1.0F,
+        2.0F, 3.0F
+    });
 }
 
 TEST_CASE("Matrix identity", "matrix")
 {
     const auto matrix = omath::Matrix<float, 2>::identity();
 
-    REQUIRE(matrix[0][0] == 1.0F);
-    REQUIRE(matrix[0][1] == 0.0F);
-    REQUIRE(matrix[1][0] == 0.0F);
-    REQUIRE(matrix[1][1] == 1.0F);
+    REQUIRE(matrix == omath::Matrix<float, 2, 2>{
+        1.0F, 0.0F,
+        0.0F, 1.0F
+    });
 }
 
 TEST_CASE("Matrix element setter", "matrix")
@@ -41,10 +41,10 @@ TEST_CASE("Matrix element setter", "matrix")
     matrix[1][0] = 3.0F;
     matrix[1][1] = 4.0F;
 
-    REQUIRE(matrix[0][0] == 1.0F);
-    REQUIRE(matrix[0][1] == 2.0F);
-    REQUIRE(matrix[1][0] == 3.0F);
-    REQUIRE(matrix[1][1] == 4.0F);
+    REQUIRE(matrix == omath::Matrix<float, 2, 2>{
+        1.0F, 2.0F,
+        3.0F, 4.0F
+    });
 }
 
 TEST_CASE("Matrix comparison operators", "vector")
@@ -71,7 +71,9 @@ TEST_CASE("1x1 matrix transpose", "matrix")
     omath::Matrix<float, 1> matrix{1.0F};
     matrix.transpose();
 
-    REQUIRE(matrix[0][0] == 1.0F);
+    REQUIRE(matrix == omath::Matrix<float, 1, 1>{
+        1.0F
+    });
 }
 
 TEST_CASE("2x2 matrix transpose", "matrix")
@@ -82,10 +84,10 @@ TEST_CASE("2x2 matrix transpose", "matrix")
     };
     matrix.transpose();
 
-    REQUIRE(matrix[0][0] == 1.0F);
-    REQUIRE(matrix[0][1] == 3.0F);
-    REQUIRE(matrix[1][0] == 2.0F);
-    REQUIRE(matrix[1][1] == 4.0F);
+    REQUIRE(matrix == omath::Matrix<float, 2, 2>{
+        1.0F, 3.0F,
+        2.0F, 4.0F
+    });
 }
 
 TEST_CASE("3x3 matrix transpose", "matrix")
@@ -97,15 +99,11 @@ TEST_CASE("3x3 matrix transpose", "matrix")
     };
     matrix.transpose();
 
-    REQUIRE(matrix[0][0] == 1.0F);
-    REQUIRE(matrix[0][1] == 4.0F);
-    REQUIRE(matrix[0][2] == 7.0F);
-    REQUIRE(matrix[1][0] == 2.0F);
-    REQUIRE(matrix[1][1] == 6.0F);
-    REQUIRE(matrix[1][2] == 8.0F);
-    REQUIRE(matrix[2][0] == 3.0F);
-    REQUIRE(matrix[2][1] == 6.0F);
-    REQUIRE(matrix[2][2] == 9.0F);
+    REQUIRE(matrix == omath::Matrix<float, 3, 3>{
+        1.0F, 4.0F, 7.0F,
+        2.0F, 6.0F, 8.0F,
+        3.0F, 6.0F, 9.0F
+    });
 }
 
 TEST_CASE("4x4 matrix transpose", "matrix")
@@ -118,22 +116,12 @@ TEST_CASE("4x4 matrix transpose", "matrix")
     };
     matrix.transpose();
 
-    REQUIRE(matrix[0][0] == 1.0F);
-    REQUIRE(matrix[0][1] == 5.0F);
-    REQUIRE(matrix[0][2] == 9.0F);
-    REQUIRE(matrix[0][3] == 13.0F);
-    REQUIRE(matrix[1][0] == 2.0F);
-    REQUIRE(matrix[1][1] == 6.0F);
-    REQUIRE(matrix[1][2] == 10.0F);
-    REQUIRE(matrix[1][3] == 14.0F);
-    REQUIRE(matrix[2][0] == 3.0F);
-    REQUIRE(matrix[2][1] == 7.0F);
-    REQUIRE(matrix[2][2] == 11.0F);
-    REQUIRE(matrix[2][3] == 15.0F);
-    REQUIRE(matrix[3][0] == 4.0F);
-    REQUIRE(matrix[3][1] == 8.0F);
-    REQUIRE(matrix[3][2] == 12.0F);
-    REQUIRE(matrix[3][3] == 16.0F);
+    REQUIRE(matrix == omath::Matrix<float, 4, 4, false>{
+        1.0F, 5.0F, 9.0F, 13.0F,
+        2.0F, 6.0F, 10.0F, 14.0F,
+        3.0F, 7.0F, 11.0F, 15.0F,
+        4.0F, 8.0F, 12.0F, 16.0F
+    });
 }
 
 TEST_CASE("4x4 matrix transpose using SIMD", "matrix")
@@ -146,22 +134,12 @@ TEST_CASE("4x4 matrix transpose using SIMD", "matrix")
     };
     matrix.transpose();
 
-    REQUIRE(matrix[0][0] == 1.0F);
-    REQUIRE(matrix[0][1] == 5.0F);
-    REQUIRE(matrix[0][2] == 9.0F);
-    REQUIRE(matrix[0][3] == 13.0F);
-    REQUIRE(matrix[1][0] == 2.0F);
-    REQUIRE(matrix[1][1] == 6.0F);
-    REQUIRE(matrix[1][2] == 10.0F);
-    REQUIRE(matrix[1][3] == 14.0F);
-    REQUIRE(matrix[2][0] == 3.0F);
-    REQUIRE(matrix[2][1] == 7.0F);
-    REQUIRE(matrix[2][2] == 11.0F);
-    REQUIRE(matrix[2][3] == 15.0F);
-    REQUIRE(matrix[3][0] == 4.0F);
-    REQUIRE(matrix[3][1] == 8.0F);
-    REQUIRE(matrix[3][2] == 12.0F);
-    REQUIRE(matrix[3][3] == 16.0F);
+    REQUIRE(matrix == omath::Matrix<float, 4, 4, true>{
+        1.0F, 5.0F, 9.0F, 13.0F,
+        2.0F, 6.0F, 10.0F, 14.0F,
+        3.0F, 7.0F, 11.0F, 15.0F,
+        4.0F, 8.0F, 12.0F, 16.0F
+    });
 }
 
 TEST_CASE("1x1 matrix determinant", "matrix")
@@ -183,26 +161,6 @@ TEST_CASE("2x2 matrix determinant", "matrix")
     REQUIRE(matrix.determinant() == Approx(-2.0F));
 }
 
-TEST_CASE("2x2 matrix comparison", "matrix")
-{
-    const omath::Matrix<float, 2> matrix1{
-        0.0F, 1.0F,
-        2.0F, 3.0F
-    };
-
-    const omath::Matrix<float, 2> matrix2{
-        0.0F, 1.0F,
-        2.0F, 3.0F
-    };
-
-    const omath::Matrix<float, 2> matrix3{
-        1.0F, 2.0F,
-        3.0F, 4.0F
-    };
-
-    REQUIRE(matrix1 == matrix2);
-    REQUIRE(matrix1 != matrix3);
-}
 
 TEST_CASE("2x2 matrix negation", "matrix")
 {
@@ -921,10 +879,10 @@ TEST_CASE("2x2 matrix division assignment with scalar", "matrix")
 
     matrix /= 2.0F;
 
-    REQUIRE(matrix[0][0] == 1.0F);
-    REQUIRE(matrix[0][1] == 2.0F);
-    REQUIRE(matrix[1][0] == 2.0F);
-    REQUIRE(matrix[1][1] == 5.0F);
+    REQUIRE(matrix == omath::Matrix<float, 2, 2>{
+        1.0F, 2.0F,
+        2.0F, 5.0F
+    });
 }
 
 TEST_CASE("4x4 matrix division assignment with scalar", "matrix")
@@ -938,25 +896,12 @@ TEST_CASE("4x4 matrix division assignment with scalar", "matrix")
 
     matrix /= 2.0F;
 
-    REQUIRE(matrix[0][0] == 0.0F);
-    REQUIRE(matrix[0][1] == 1.0F);
-    REQUIRE(matrix[0][2] == 0.0F);
-    REQUIRE(matrix[0][3] == 1.0F);
-
-    REQUIRE(matrix[1][0] == 1.0F);
-    REQUIRE(matrix[1][1] == -2.0F);
-    REQUIRE(matrix[1][2] == 1.0F);
-    REQUIRE(matrix[1][3] == -2.0F);
-
-    REQUIRE(matrix[2][0] == 0.0F);
-    REQUIRE(matrix[2][1] == 1.0F);
-    REQUIRE(matrix[2][2] == 0.0F);
-    REQUIRE(matrix[2][3] == 1.0F);
-
-    REQUIRE(matrix[3][0] == 1.0F);
-    REQUIRE(matrix[3][1] == -2.0F);
-    REQUIRE(matrix[3][2] == 1.0F);
-    REQUIRE(matrix[3][3] == -2.0F);
+    REQUIRE(matrix == omath::Matrix<float, 4, 4, false>{
+        0.0F, 1.0F, 0.0F, 1.0F,
+        1.0F, -2.0F, 1.0F, -2.0F,
+        0.0F, 1.0F, 0.0F, 1.0F,
+        1.0F, -2.0F, 1.0F, -2.0F
+    });
 }
 
 TEST_CASE("4x4 matrix division assignment with scalar using SIMD", "matrix")
@@ -970,25 +915,12 @@ TEST_CASE("4x4 matrix division assignment with scalar using SIMD", "matrix")
 
     matrix /= 2.0F;
 
-    REQUIRE(matrix[0][0] == 0.0F);
-    REQUIRE(matrix[0][1] == 1.0F);
-    REQUIRE(matrix[0][2] == 0.0F);
-    REQUIRE(matrix[0][3] == 1.0F);
-
-    REQUIRE(matrix[1][0] == 1.0F);
-    REQUIRE(matrix[1][1] == -2.0F);
-    REQUIRE(matrix[1][2] == 1.0F);
-    REQUIRE(matrix[1][3] == -2.0F);
-
-    REQUIRE(matrix[2][0] == 0.0F);
-    REQUIRE(matrix[2][1] == 1.0F);
-    REQUIRE(matrix[2][2] == 0.0F);
-    REQUIRE(matrix[2][3] == 1.0F);
-
-    REQUIRE(matrix[3][0] == 1.0F);
-    REQUIRE(matrix[3][1] == -2.0F);
-    REQUIRE(matrix[3][2] == 1.0F);
-    REQUIRE(matrix[3][3] == -2.0F);
+    REQUIRE(matrix == omath::Matrix<float, 4, 4, true>{
+        0.0F, 1.0F, 0.0F, 1.0F,
+        1.0F, -2.0F, 1.0F, -2.0F,
+        0.0F, 1.0F, 0.0F, 1.0F,
+        1.0F, -2.0F, 1.0F, -2.0F
+    });
 }
 
 TEST_CASE("Scalar multiplication with 2x2 matrix", "matrix")
@@ -1000,10 +932,10 @@ TEST_CASE("Scalar multiplication with 2x2 matrix", "matrix")
 
     const auto result = 2.0F * matrix;
 
-    REQUIRE(result[0][0] == 4.0F);
-    REQUIRE(result[0][1] == 6.0F);
-    REQUIRE(result[1][0] == 8.0F);
-    REQUIRE(result[1][1] == 10.0F);
+    REQUIRE(result == omath::Matrix<float, 2, 2>{
+        4.0F, 6.0F,
+        8.0F, 10.0F
+    });
 }
 
 TEST_CASE("1x1 matrix multiplication", "matrix")
@@ -1018,7 +950,7 @@ TEST_CASE("1x1 matrix multiplication", "matrix")
 
     const auto result = matrix1 * matrix2;
 
-    REQUIRE(result[0][0] == 6.0F);
+    REQUIRE(result == omath::Matrix<float, 1, 1>{6.0F});
 }
 
 TEST_CASE("2x2 matrix multiplication", "matrix")
