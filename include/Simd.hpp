56e72b9beacbd722349123a99603ae268ce0ae25
Elviss Strazdins <elviss@elviss.lv>
2021-10-07 03:04:06 +0300

    Add defines for SSE2

--- include/Simd.hpp
+++ include/Simd.hpp
@@ -6,10 +6,14 @@
 #define OMATH_SIMD
 
 #ifndef OMATH_DISABLE_SIMD
-#  if defined(__SSE__) || defined(_M_X64) || _M_IX86_FP > 0
+#  if defined(__SSE__) || defined(_M_X64) || _M_IX86_FP >= 1
 #    include <xmmintrin.h>
 #    define OMATH_SIMD_SSE
-#  elif defined(__ARM_NEON__)
+#  endif
+#  if defined(__SSE2__) || defined(_M_X64) || _M_IX86_FP >= 2
+#    define OMATH_SIMD_SSE2
+#  endif
+#  if defined(__ARM_NEON__)
 #    include <arm_neon.h>
 #    define OMATH_SIMD_NEON
 #  endif
