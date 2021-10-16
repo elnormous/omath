229aa20cb8898e49a58a4abf4388253305c1a846
Elviss Strazdins <elviss@elviss.lv>
2021-10-16 12:00:08 +0300

    Add macro for SSE3

--- include/Simd.hpp
+++ include/Simd.hpp
@@ -12,6 +12,9 @@
 #  if defined(__SSE2__) || defined(_M_X64) || _M_IX86_FP >= 2
 #    define OMATH_SIMD_SSE2
 #  endif
+#  if defined(__SSE3__)
+#    define OMATH_SIMD_SSE3
+#  endif
 #  if defined(__AVX__)
 #    define OMATH_SIMD_AVX
 #  endif
