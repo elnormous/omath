27e03084e1ac56fedb0600b42392081efc071242
Elviss Strazdins <elviss@elviss.lv>
2021-10-31 03:39:00 +0300

    Implement dot, length, and lengthSquared using SSE3

--- include/VectorSse.hpp
+++ include/VectorSse.hpp
@@ -15,6 +15,10 @@
 #  include <emmintrin.h>
 #endif
 
+#ifdef __SSE3__
+#  include <pmmintrin.h>
+#endif // __SSE3__
+
 namespace omath
 {
 #if defined(__SSE__) || defined(_M_X64) || _M_IX86_FP >= 1
@@ -106,42 +110,38 @@ namespace omath
         return vector;
     }
 
+#  ifndef __SSE3__
     template <>
     [[nodiscard]] inline auto length(const Vector<float, 4>& vector) noexcept
     {
-        float result;
         const auto v = _mm_load_ps(vector.v.data());
         const auto t1 = _mm_mul_ps(v, v);
         const auto t2 = _mm_add_ps(t1, _mm_shuffle_ps(t1, t1, _MM_SHUFFLE(2, 1, 0, 3)));
         const auto t3 = _mm_add_ps(t2, _mm_shuffle_ps(t2, t2, _MM_SHUFFLE(1, 0, 3, 2)));
         const auto s = _mm_sqrt_ps(t3);
-        result = _mm_cvtss_f32(s);
-        return result;
+        return _mm_cvtss_f32(s);
     }
 
     template <>
     [[nodiscard]] inline auto lengthSquared(const Vector<float, 4>& vector) noexcept
     {
-        float result;
         const auto v = _mm_load_ps(vector.v.data());
         const auto t1 = _mm_mul_ps(v, v);
         const auto t2 = _mm_add_ps(t1, _mm_shuffle_ps(t1, t1, _MM_SHUFFLE(2, 1, 0, 3)));
         const auto t3 = _mm_add_ps(t2, _mm_shuffle_ps(t2, t2, _MM_SHUFFLE(1, 0, 3, 2)));
-        result = _mm_cvtss_f32(t3);
-        return result;
+        return _mm_cvtss_f32(t3);
     }
 
     template <>
     [[nodiscard]] inline auto dot(const Vector<float, 4>& vector1,
                                   const Vector<float, 4>& vector2) noexcept
     {
-        float result;
         const auto t1 = _mm_mul_ps(_mm_load_ps(vector1.v.data()), _mm_load_ps(vector2.v.data()));
         const auto t2 = _mm_add_ps(t1, _mm_shuffle_ps(t1, t1, _MM_SHUFFLE(2, 1, 0, 3)));
         const auto t3 = _mm_add_ps(t2, _mm_shuffle_ps(t2, t2, _MM_SHUFFLE(1, 0, 3, 2)));
-        result = _mm_cvtss_f32(t3);
-        return result;
+        return _mm_cvtss_f32(t3);
     }
+#  endif // __SSE3__
 #endif // SSE
 
 #if defined(__SSE2__) || defined(_M_X64) || _M_IX86_FP >= 2
@@ -245,6 +245,39 @@ namespace omath
     }
 #  endif // __AVX__
 #endif // SSE2
+
+#ifdef __SSE3__
+    template <>
+    [[nodiscard]] inline auto length(const Vector<float, 4>& vector) noexcept
+    {
+        const auto v = _mm_load_ps(vector.v.data());
+        const auto t1 = _mm_mul_ps(v, v);
+        const auto t2 = _mm_hadd_ps(t1, t1);
+        const auto t3 = _mm_hadd_ps(t2, t2);
+        const auto s = _mm_sqrt_ps(t3);
+        return _mm_cvtss_f32(s);
+    }
+
+    template <>
+    [[nodiscard]] inline auto lengthSquared(const Vector<float, 4>& vector) noexcept
+    {
+        const auto v = _mm_load_ps(vector.v.data());
+        const auto t1 = _mm_mul_ps(v, v);
+        const auto t2 = _mm_hadd_ps(t1, t1);
+        const auto t3 = _mm_hadd_ps(t2, t2);
+        return _mm_cvtss_f32(t3);
+    }
+
+    template <>
+    [[nodiscard]] inline auto dot(const Vector<float, 4>& vector1,
+                                  const Vector<float, 4>& vector2) noexcept
+    {
+        const auto t1 = _mm_mul_ps(_mm_load_ps(vector1.v.data()), _mm_load_ps(vector2.v.data()));
+        const auto t2 = _mm_hadd_ps(t1, t1);
+        const auto t3 = _mm_hadd_ps(t2, t2);
+        return _mm_cvtss_f32(t3);
+    }
+#endif // __SSE3__
 }
 
 #endif // OMATH_VECTOR_SSE
